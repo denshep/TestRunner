d98305a9a1584fd979a549a545ddb4f5d0ac1c77
Denis Shepelev <den.shep@yahoo.com>
2015-02-07 00:59:23 -0800

    added test runner. My runner launhes fat jar

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#============================================
+GITHUB_ACCOUNT=denshep
+WS_DIR=Workspace
+REPO_NAME=Single_Title_Validation
+APP_VERSION=1.1
+MAIN_CLASS=Selenium
+#ARGS_01=
+#============================================
+if ! which java >/dev/null 2>&1 ; then echo Java not installed; return; fi
+	if ! which mvn >/dev/null 2>&1 ; then echo Maven not installed; return; fi
+		if ! which git >/dev/null 2>&1 ; then echo Git not installed; return; fi
+			
+			if [ -d "$HOME/$WS_DIR" ]; then cd ~/$WS_DIR; else echo $WS_DIR directory does not exist; return; fi
+				if [ -d "$HOME/$WS_DIR/$REPO_NAME" ]; then rm -rf $HOME/$WS_DIR/$REPO_NAME; fi
+					git clone https://github.com/$GITHUB_ACCOUNT/$REPO_NAME.git
+					
+					cd ./$REPO_NAME
+					
+					mvn package -Dbuild.version=$APP_VERSION
+					echo "Executing java program . . ."
+					java -jar $HOME/$WS_DIR/$REPO_NAME/target/$REPO_NAME-$APP_VERSION-jar-with-dependencies.jar $ARGS_01
